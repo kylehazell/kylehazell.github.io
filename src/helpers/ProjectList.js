3910634c53d32261ff3fce789e2b79d772d0e855
kylehazell <khazell415@gmail.com>
2022-08-11 11:36:45 -0700

    done

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,37 @@
+import appleGame from '../assets/apple.png';
+import antGame from '../assets/antPath.png';
+import ACR from '../assets/login.png';
+import Smoke from '../assets/punkSmoke.jpg';
+import Ball from '../assets/superBall.png';
+
+export const ProjectList=[
+
+    {
+        name:"Apple Farmer",
+        image:appleGame,
+        skills: "Unity,C#",
+
+    },
+    {
+        name:"Jump Smoke",
+        image:Smoke,
+        skills: "Unity,C#",
+    },
+    {
+        name:"Iventory Website/Senior Project",
+        image:ACR,
+        skills: "HTML, Javascript, CSS, Django, SQL, Heroku",
+    },
+    {
+        name:"The Ant Path ",
+        image:antGame,
+        skills: "Java,JRE,CodeNameOne, "
+    },
+    {
+        name:"Rolling Thunder",
+        image:Ball,
+        skills: "Unity,C#",
+    }
+
+
+];
